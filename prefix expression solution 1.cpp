46d72d9024cd91395ef53f8c16d36a25ec040872
你猜 <117290962+GoldenStain@users.noreply.github.com>
2023-09-16 15:59:53 +0800

    Create prefix expression solution 1.cpp

---
+++ prefix expression solution 1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+char a[20];
+double expt()
+{
+    scanf("%s",a);
+    switch(a[0])
+    {
+        case '+':
+            return expt()+expt();
+        case '-':
+            return expt()-expt();
+        case '*':
+            return expt()*expt();
+        case '/':
+            return expt()/expt();
+        default:
+            return atof(a);
+    }
+}
+int main()
+{
+ 
+    double ans=0;
+    ans=expt();
+    printf("%lf\n",ans);
+    return 0;
+}
