f8827bf53d2868feba6cf58590533b75bf36bc22
你猜 <117290962+GoldenStain@users.noreply.github.com>
2023-09-18 14:59:07 +0800

    Create postfix expression solution.cpp

---
+++ postfix expression solution.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
+using namespace std;
+
+const int N = 1000005;
+
+char a[20];
+int top;
+bool expression[N];//表达式栈
+stack<char> op;//符号栈
+stack<double> num;//数字栈
+
+inline void calc()
+{
+    double b = num.top();
+    num.pop();
+    double a = num.top();
+    num.pop();
+    char c = op.top();
+    op.pop();
+    double res;
+    if(c == '+') res = a + b;
+    if(c == '-') res = a - b;
+    if(c == '*') res = a * b;
+    if(c == '/') res = a / b;
+    num.push(res);
+    top -= 3;
+    expression[++top] = 1;
+}
+
+int main()
+{
+    while(scanf("%s", a) != -1)
+    {
+        if(a[0] == '-' || a[0] == '+' || a[0] == '/' || a[0] == '*')
+        {
+            expression[++top] = 0;
+            op.push(a[0]);
+            while(!expression[top])
+            {
+                calc();
+            }
+        }
+        else 
+        {
+            double n = atof(a);
+            expression[++top] = 1;
+            num.push(n);
+        }
+    }
+    printf("%lf\n", num.top());
+    return 0;
+}
